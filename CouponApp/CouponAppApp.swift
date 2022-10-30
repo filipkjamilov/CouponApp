65dac9a9aab453a93e0c661d211c4811d76d7fa5
Filip Kjamilov <filipkjamilov@gmail.com>
2022-10-30 10:46:02 +0100

    Initial commit

---
+++ CouponApp/CouponAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouponAppApp.swift
+//  CouponApp
+//
+//  Created by Filip Kjamilov on 30.10.22.
+//
+
+import SwiftUI
+
+@main
+struct CouponAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
